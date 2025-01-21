38553dbf2394e40401ed5af9cca72229970d7f5a
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-01-20 16:30:02 -0800

    Add index.html, style.css, script.js as well as main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# --- Section #23: Git and Github --- #
+"""
+    - git clone                             --> for clonning or copying a repository to a computer                                              
+    - git add -A or .                       --> Add changes
+    - git commit -m 'Commit message'        --> Save changes with messages
+    - git push origin main                  --> push changes to the main branch
+    - git status                            --> Shows the current state of your git working directory
+    - git pull                              --> pulls changes made by others and updates the current working dir
+    - git branch                            --> list all the branch in the repository
+    - git branch                            --> < create a new branch >
+    - git checkout                          --> < name of the branch you want to switch to >
+"""
